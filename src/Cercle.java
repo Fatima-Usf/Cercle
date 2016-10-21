6d2883f2f261a2a3f2dc0e5c83c269703b0acb76
Fatima-YF <f.ucf.eva@gmail.com>
2016-10-22 01:41:07 +0200

    Initial commit

---
+++ src/Cercle.java
@@ -0,0 +1,21 @@
+/**
+ * Created by fatima on 02/10/16.
+ */
+public class Cercle {
+    double x;
+    double y;
+    double rayon;
+    public Cercle (double a, double b, double r){
+        x=a;
+        y=b;
+        rayon =r;
+
+    }
+    static double distance ( Cercle c1, Cercle c2){
+
+        return (c1.rayon+c2.rayon);
+    }
+    static double distcentres (Cercle c1, Cercle c2){
+        return();
+    }
+}
