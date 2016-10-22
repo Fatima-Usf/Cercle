18df80a99c682e5121d53f0c22f62f28ec584323
fcmam5 <dr.fcmam5@gmail.com>
2016-10-22 18:28:37 +0100

    encapsulation, setters and getters and add todo

--- src/Cercle.java
+++ src/Cercle.java
@@ -2,20 +2,46 @@
  * Created by fatima on 02/10/16.
  */
 public class Cercle {
-    double x;
-    double y;
-    double rayon;
+    private double x;
+    private double y;
+    private double rayon;
+
     public Cercle (double a, double b, double r){
-        x=a;
-        y=b;
-        rayon =r;
-
+        this.x=a;
+        this.y=b;
+        this.rayon =r;
     }
-    static double distance ( Cercle c1, Cercle c2){
 
+    public double distance ( Cercle c1, Cercle c2){
         return (c1.rayon+c2.rayon);
     }
-    static double distcentres (Cercle c1, Cercle c2){
+
+    public double distcentres (Cercle c1, Cercle c2){
+        // TODO: complete this methode :)
         return();
+    }
+
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public double getRayon(){
+        return this.rayon;
+    }
+
+    public void setX(double val){
+        this.x=val;
+    }
+
+    public void setY(double val){
+        this.y=val;
+    }
+
+    public void setRayon(double val){
+        this.rayon=val;
     }
 }
